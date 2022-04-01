8c269daf2fba178740e3a115eaf8bbd7f1caa934
Jun Kang <jundol8791@gmail.com>
2022-04-02 00:34:37 +0900

    commit

--- src/test/java/com/example/inflean_java_test/StudyTest.java
+++ src/test/java/com/example/inflean_java_test/StudyTest.java
@@ -2,10 +2,25 @@ package com.example.inflean_java_test;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.condition.*;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.aggregator.AggregateWith;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregationException;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregator;
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.converter.SimpleArgumentConverter;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.lang.Nullable;
 
 import java.time.Duration;
 import java.util.function.Supplier;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 import static org.junit.jupiter.api.Assumptions.assumingThat;
@@ -13,10 +28,13 @@ import static org.junit.jupiter.api.Assumptions.assumingThat;
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class) // 메소드명에서 언더스코어를 공백으로 치환한다.
 class StudyTest {
 
+    int value = 1;
+
     /* Junit5 Assertion */
     @Test
-    @DisplayName("예외를 모두 실행하기")
+    //@DisplayName("예외를 모두 실행하기")
     void assert_all() {
+        System.out.println("value++ = " + value++);
         Study study = new Study(-10);
 
         assertAll(
@@ -34,6 +52,7 @@ class StudyTest {
     @Test
     @DisplayName("예외 예측하기")
     void assert_exception() {
+        System.out.println("value++ = " + value++);
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
             new Study(-10);
         });
@@ -124,6 +143,126 @@ class StudyTest {
     }
 
     /* 조건에 따라 테스트 실행하기 */
+
+    /* tagging , filtering */
+    @Test
+    @DisplayName("태그 적용하기")
+    @Tag("fast")
+    void tagging() {
+        Study study = new Study(10);
+        assertEquals(10, study.getLimit());
+    }
+    /* tagging , filtering */
+    /* custom tag */
+    @DisplayName("커스텀 태그 - fast")
+    @FastTest
+    void customTagFast() {
+        Study study = new Study(10);
+        assertEquals(10, study.getLimit());
+    }
+
+    @DisplayName("커스텀 태그 - slow")
+    @SlowTest
+    void customTagSlow() {
+        Study study = new Study(10);
+        assertEquals(10, study.getLimit());
+    }
+    /* custom tag */
+
+    /* repeat test */
+    @DisplayName("테스트 반복하기 - 1")
+    @RepeatedTest(value = 10, name = "{displayName}, {currentRepetition}/{totalRepetitions}")
+    void repeatTest_1(RepetitionInfo repetitionInfo) {
+        System.out.println("repetitionInfo.getCurrentRepetition() = " + repetitionInfo.getCurrentRepetition());
+        System.out.println("repetitionInfo.getTotalRepetitions() = " + repetitionInfo.getTotalRepetitions());
+    }
+
+    @DisplayName("테스트 반복하기 - 2")
+    @ParameterizedTest(name = "{index} {displayName} message = {0}")
+    @ValueSource(strings = {"오늘도", "재밌는", "하루", "입니다."})
+    void repeatParamTest(String message) {
+        System.out.println("message = " + message);
+    }
+
+    @DisplayName("테스트 반복하기 - 3")
+    @ParameterizedTest(name = "{index} {displayName} message = {0}")
+    @ValueSource(strings = {"오늘도", "재밌는", "하루", "입니다."})
+    @EmptySource
+    void repeatParamEmptySourceTest(String message) {
+        System.out.println("message = " + message);
+    }
+
+    @DisplayName("테스트 반복하기 - 4")
+    @ParameterizedTest(name = "{index} {displayName} message = {0}")
+    @ValueSource(strings = {"오늘도", "재밌는", "하루", "입니다."})
+    @EmptySource
+    @NullSource
+    void repeatParamNullEmptySourceTest(String message) {
+        System.out.println("message = " + message);
+    }
+
+    // SAC 변환
+    // 암묵적 변환
+    @DisplayName("테스트 반복하기 - SAC")
+    @ParameterizedTest(name = "{index} {displayName} message = {0}")
+    @ValueSource(ints = {10, 20, 40})
+    void repeatSacTest(@ConvertWith(StudyConverter.class) Study study) {
+        System.out.println("study.getLimit() = " + study.getLimit());
+    }
+
+    static class StudyConverter extends SimpleArgumentConverter {
+
+        @Override
+        protected Object convert(Object source, Class<?> targetType) throws ArgumentConversionException {
+            assertEquals(Study.class, targetType, "Can only convert to Study");
+            return new Study(Integer.parseInt(source.toString()));
+        }
+    }
+
+    @Disabled
+    @DisplayName("테스트 반복하기 - CsvSource - 1")
+    @ParameterizedTest(name = "{index} {displayName} message = {0}")
+    @CsvSource({"10, '자바 스터디'", "20, 스프링"})
+    void repeatCsvSourceTest(ArgumentsAccessor argumentsAccessor) {
+        Study study = new Study(argumentsAccessor.getInteger(0), argumentsAccessor.getString(1));
+        System.out.println("study = " + study);
+    }
+
+    @DisplayName("테스트 반복하기 - CsvSource - 2")
+    @ParameterizedTest(name = "{index} {displayName} message = {0}")
+    @CsvSource({"10, '자바 스터디'", "20, 스프링"})
+    void repeatCsvSourceAggrTest(@AggregateWith(StudyAggregator.class) Study study) {
+        System.out.println("study = " + study);
+    }
+
+    static class StudyAggregator implements ArgumentsAggregator {
+
+        @Override
+        public Object aggregateArguments(ArgumentsAccessor accessor, ParameterContext context) throws ArgumentsAggregationException {
+            return new Study(accessor.getInteger(0), accessor.getString(1));
+        }
+    }
+
+    /* repeat test */
+
+    /* test instance */
+
+    // 테스트 클래스는 테스트 메서드마다 인스턴스가 새로 생성된다.
+    // 이유는 테스트간 의존성을 없애기 위함.
+    // 테스트 순서는 코드의 위치에 의존해서 매번 순서에 맞게끔 실행되는걸 보장할 수는 없음.
+    // JUnit5 부터는 기존 전략을 변경하는 방법이 추가됌. 클래스에 @TestInstance(TestInstance.Lifecycle.PER_CLASS) 선언하면 클래스당 인스턴스가 된다.
+    // 선언시 beforeAll, afterAll 이 static 일 필요가 없어짐.
+
+    /* test instance */
+
+    /* test order 지정 */
+
+    // 클래스에 @TestMethodOrder(MethodOrderer.OrderAnnotation.class) 선언
+    // 메소드에 @Order(1) 선언.
+    // 순서 지정이 가능
+
+    /* test order 지정 */
+
 
     @BeforeAll
     static void beforeAll() {
